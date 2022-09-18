1ac89f92382bc53731b1c54f077eaf67992003df
ckost9n <ckost9n@gmail.com>
2022-09-18 16:05:40 +0300

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by Konstantin on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
